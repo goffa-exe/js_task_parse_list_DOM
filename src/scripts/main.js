31818dd2d47537b0ad7fec9e57559c3d0788b8d0
gOFFa <n4v4rm0re666@gmail.com>
2025-08-11 14:32:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write code here
+const listOfEmployees = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+function sortList(list) {
+  const sortedList = [...list].sort(
+    (obj1, obj2) =>
+      dataToNumber(obj2.dataset.salary) - dataToNumber(obj1.dataset.salary),
+  );
+
+  return sortedList;
+}
+
+function getEmplyees(list) {
+  const employees = [...list].map((person) => {
+    return {
+      name: person.textContent.trim(),
+      position: person.dataset.position,
+      salary: Number(person.dataset.salary.slice(1).replace(/,/g, '')),
+      age: Number(person.dataset.age),
+    };
+  });
+
+  return employees;
+}
+
+function dataToNumber(dataAttribute) {
+  return Number(dataAttribute.slice(1).replace(/,/g, ''));
+}
+
+const sortedEmployess = sortList(listOfEmployees);
+
+sortedEmployess.forEach((element) => {
+  ul.append(element);
+});
+getEmplyees(listOfEmployees);
+sortList(listOfEmployees);
