3dbe49140bea116dc4f131d56c8a66799cf1d905
gOFFa <n4v4rm0re666@gmail.com>
2025-08-11 14:50:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,38 @@
 'use strict';
 
 // write code here
-const listOfEmployees = document.querySelectorAll('li');
 const ul = document.querySelector('ul');
 
 function sortList(list) {
-  const sortedList = [...list].sort(
+  const listItems = list.querySelectorAll('li');
+
+  return [...listItems].sort(
     (obj1, obj2) =>
       dataToNumber(obj2.dataset.salary) - dataToNumber(obj1.dataset.salary),
   );
-
-  return sortedList;
 }
 
-function getEmplyees(list) {
-  const employees = [...list].map((person) => {
+function getEmployees(list) {
+  const listItems = list.querySelectorAll('li');
+
+  return [...listItems].map((person) => {
     return {
       name: person.textContent.trim(),
       position: person.dataset.position,
-      salary: Number(person.dataset.salary.slice(1).replace(/,/g, '')),
+      salary: dataToNumber(person.dataset.salary.slice(1).replace(/,/g, '')),
       age: Number(person.dataset.age),
     };
   });
-
-  return employees;
 }
 
 function dataToNumber(dataAttribute) {
   return Number(dataAttribute.slice(1).replace(/,/g, ''));
 }
 
-const sortedEmployess = sortList(listOfEmployees);
+const sortedEmployess = sortList(ul);
+
+getEmployees(ul);
 
 sortedEmployess.forEach((element) => {
   ul.append(element);
 });
-getEmplyees(listOfEmployees);
-sortList(listOfEmployees);
